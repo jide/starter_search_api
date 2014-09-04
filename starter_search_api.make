d74b81f2b135a23f793ed116df85d2513f7af053
jide <jide.fr@gmail.com>
2014-09-04 16:22:11 +0200

    Building features

---
+++ starter_search_api.make
@@ -0,0 +1,6 @@
+core = "7.x"
+api = "2"
+
+projects[search_api][subdir] = "contrib"
+projects[search_api_db][subdir] = "contrib"
+projects[search_api_views][subdir] = "contrib"
---
+++ templates/block--views---exp-search-autocomplete-block.tpl.php
@@ -0,0 +1,2 @@
+<div id="<?php print $block_html_id; ?>" class="<?php print $classes; ?> navbar-form search-autocomplete"<?php print $attributes; ?>>
+  <?php print $content ?>
